5fe5edd32bd860a877d9fa054e7c984d52233291
Huy ROR <huytran0001010@gmail.com>
2016-09-26 11:45:50 +0000

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User <ActiveRecord::Base
-    
+    validates :username, presence: true, 
+                uniqueness: {case_sensitive: false},
+                length: {minimum: 3, maximum: 25}
+                
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :emai, presence: true,
+                length: {maximum:200},
+                uniqueness : {case_sensitive: false},
+                format: {with: VALID_EMAIL_REGEX}
     
     
 end
